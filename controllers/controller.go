6daa1d015376bc30daf91fd8e73a941b7b28ded0
Jonas Karlsson <jonas.karlsson@stim.se>
2014-11-25 09:04:23 +0100

    more granular error handling

--- controllers/controller.go
+++ controllers/controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+    "errors"
 )
 
 type Action func(rw http.ResponseWriter, r *http.Request) error
@@ -11,7 +12,20 @@ type AppController struct{}
 func (c *AppController) Action(a Action) func(rw http.ResponseWriter, r *http.Request) {
     return func(rw http.ResponseWriter, r *http.Request) {
         if err := a(rw, r); err != nil {
-            http.Error(rw, err.Error(), http.StatusInternalServerError)
+            switch err {
+            case Err400:
+                http.Error(rw, err.Error(), http.StatusBadRequest)
+            case Err404:
+                http.Error(rw, err.Error(), http.StatusNotFound)
+            default:
+                http.Error(rw, err.Error(), http.StatusInternalServerError)
+            }
         }
     }
 }
+
+var (
+    Err404 = errors.New("Not found")
+    Err400 = errors.New("Bad input")
+    Err500 = errors.New("Internal Server Error")
+)
